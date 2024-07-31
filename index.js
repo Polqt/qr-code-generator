c21d5fc197d24d3d747759b23161cb454cd48c37
Janpol Hidalgo <poyhidalgo@gmail.com>
2024-07-31 20:05:23 +0800

    Adding ejs module

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ import bodyParser from 'body-parser';
 const app = express();
 const port = 3000;
 
-
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get("/", (req, res) => {
     res.render("index.ejs")
 })
-
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}.`)
