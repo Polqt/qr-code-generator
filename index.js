f0d849dfd77a4bf8249ec9cf91d33618e056d1c5
Janpol Hidalgo <poyhidalgo@gmail.com>
2024-08-04 23:39:00 +0800

    Importing the inquirer, qr, and fs package. Also, removing the qrcode package.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import QRCode from 'qrcode';
+import inquirer from 'inquirer'
+import qr from 'qr-image'
+import fs from 'fs';
 
 const app = express();
 const port = 3000;
@@ -10,6 +12,22 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get("/", (req, res) => {
     res.render("index.ejs")
+        QRCode.toDataURL('I am a pony!')
+        .then(url => {
+            console.log(url)
+        })
+        .catch(err => {
+            console.error(err)
+        })
+
+        // With async/await
+        const generateQR = async text => {
+        try {
+            console.log(await QRCode.toDataURL(text))
+          } catch (err) {
+            console.error(err)
+        }
+    }
 })
 
 app.listen(port, () => {
