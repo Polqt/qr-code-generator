036f97cc4bbe9c2308c969fff51b1cd5d371ec36
Janpol Hidalgo <poyhidalgo@gmail.com>
2024-08-05 00:07:46 +0800

    Adjusting the size and finishing up the back end.

--- index.js
+++ index.js
@@ -1,35 +1,31 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import inquirer from 'inquirer'
-import qr from 'qr-image'
-import fs from 'fs';
+import qr from 'qr-image';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
 const port = 3000;
 
-app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extended: true }))
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.get("/", (req, res) => {
-    res.render("index.ejs")
-        QRCode.toDataURL('I am a pony!')
-        .then(url => {
-            console.log(url)
-        })
-        .catch(err => {
-            console.error(err)
-        })
+    res.render("index", { qrCodeURL: null });
+});
 
-        // With async/await
-        const generateQR = async text => {
-        try {
-            console.log(await QRCode.toDataURL(text))
-          } catch (err) {
-            console.error(err)
-        }
-    }
-})
+app.post("/generate", (req, res) => {
+    const { link } = req.body;
+    const qrImage = qr.imageSync(link, { type: 'png' });
+    const qrCodeURL = `data:image/png;base64,${qrImage.toString('base64')}`;
+    res.render("index", { qrCodeURL });
+});
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}.`)
-})
+    console.log(`Server running on port ${port}.`);
+});
